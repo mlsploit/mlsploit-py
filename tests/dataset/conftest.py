d3c2b099783dc39c9f14feb582f2464ac2bc1532
Nilaksh Das <nilakshdas@gatech.edu>
2020-03-19 19:47:26 -0400

    Add tests for dataset interface

---
+++ tests/dataset/conftest.py
@@ -0,0 +1,139 @@
+# pylint: disable=redefined-outer-name
+
+from pathlib import Path
+import random
+import string
+
+import numpy as np
+from numpy import random as np_random
+import pytest
+
+from mlsploit.dataset import Dataset
+
+from .constants import *
+
+
+@pytest.fixture
+def tmp_dataset_path(tmp_path) -> Path:
+    dataset_dir = tmp_path/'testdata'
+    dataset_dir.mkdir()
+    dataset_path = dataset_dir/RECOMMENDED_FILENAME
+    return dataset_path
+
+
+@pytest.fixture
+def make_random_valid_identifier():
+    def __make_random_valid_identifier():
+        valid_chars = string.ascii_letters + string.digits + '_'
+
+        identifier = random.choice(string.ascii_lowercase)
+        identifier += ''.join([random.choice(valid_chars)
+                               for _ in range(random.randint(1, 5))])
+        return identifier
+
+    return __make_random_valid_identifier
+
+
+@pytest.fixture
+def make_random_item_attr(make_random_valid_identifier):
+    def __make_random_item_attr():
+        name = make_random_valid_identifier()
+        shape = None if random.random() < 0.2 \
+            else tuple(random.randint(1, 50)
+                       for _ in range(random.randint(1, 4)))
+        dtype = random.choice([str, int, float, bool, np.uint8, np.float32])
+
+        return Dataset.ItemAttr(
+            name=name,
+            shape=shape,
+            dtype=dtype)
+
+    return __make_random_item_attr
+
+
+@pytest.fixture
+def make_random_data():
+    def __make_random_data(shape, dtype):
+        data = np_random.random(shape)
+        data = data * random.randint(1, 1000)
+        data = data.astype(dtype) \
+            if isinstance(data, np.ndarray) \
+            else dtype(data)
+        return data
+
+    return __make_random_data
+
+
+@pytest.fixture
+def make_random_item_dict(make_random_data):
+    def __make_random_item(item_attrs):
+        item_dict = dict()
+        for item_attr in item_attrs:
+            attr_name = item_attr.name
+            attr_shape = item_attr.shape
+            attr_dtype = item_attr.dtype
+
+            item_dict[attr_name] = \
+                make_random_data(attr_shape, attr_dtype)
+        return item_dict
+
+    return __make_random_item
+
+
+@pytest.fixture
+def make_random_item_dicts(make_random_item_dict):
+    def __make_random_item_dicts(item_attrs):
+        num_items = random.randint(1, 20)
+        return [make_random_item_dict(item_attrs)
+                for _ in range(num_items)]
+
+    return __make_random_item_dicts
+
+
+@pytest.fixture
+def random_item_attrs(make_random_item_attr):
+    num_item_attrs = random.randint(1, 5)
+    item_attrs = [make_random_item_attr() for _ in range(num_item_attrs)]
+    item_attrs = {it.name: it for it in item_attrs} # drop duplicates
+    return list(item_attrs.values())
+
+
+@pytest.fixture
+def random_metadata(make_random_valid_identifier):
+    keys = [make_random_valid_identifier()
+            for _ in range(random.randint(1, 10))]
+
+    metadata = dict()
+    for k in keys:
+        type_ = random.choice([str, int, float, bool])
+
+        if type_ is str:
+            metadata[k] = ''.join([random.choice(string.printable)
+                                   for _ in range(random.randint(1, 100))])
+
+        elif type_ is int:
+            metadata[k] = random.randint(1, 1000)
+
+        elif type_ is float:
+            metadata[k] = random.random() * random.randint(1, 1000)
+
+        elif type_ is bool:
+            metadata[k] = random.choice([True, False])
+
+    return metadata
+
+
+@pytest.fixture
+def random_empty_dataset(tmp_dataset_path, random_item_attrs, random_metadata):
+    return Dataset(tmp_dataset_path, random_item_attrs,
+                   metadata=random_metadata)
+
+
+@pytest.fixture
+def random_dataset_with_random_data(make_random_item_dict, random_empty_dataset):
+    dataset = random_empty_dataset
+    num_items = random.randint(1, 20)
+    for _ in range(num_items):
+        dataset.add_item(**make_random_item_dict(dataset.item_attrs))
+
+    return dataset
---
+++ tests/dataset/test_dataset.py
@@ -0,0 +1,172 @@
+from devtools import debug
+import numpy as np
+from pydantic.error_wrappers import ValidationError
+import pytest
+
+from mlsploit.dataset import Dataset
+
+from .constants import *
+
+
+def test_dataset_metaclass():
+    assert Dataset.recommended_filename == RECOMMENDED_FILENAME
+
+    with pytest.raises(AttributeError) as excinfo:
+        Dataset.recommended_filename = 'something'
+    assert 'can\'t set attribute' in str(excinfo)
+
+
+def test_dataset_item_attr_init():
+    attr = Dataset.ItemAttr(
+        name='vector',
+        shape=(123,),
+        dtype=int)
+    assert attr.name == 'vector'
+    assert attr.shape == (123,)
+    assert attr.dtype is int
+
+
+def test_dataset_item_attr_immutable():
+    attr = Dataset.ItemAttr(
+        name='tensor',
+        shape=(123, 456, 789),
+        dtype=int)
+    assert attr.name == 'tensor'
+    assert attr.shape == (123, 456, 789)
+    assert attr.dtype is int
+
+    with pytest.raises(TypeError) as excinfo:
+        attr.dtype = str
+    assert '"ItemAttr" is immutable' in str(excinfo)
+
+
+@pytest.mark.parametrize('attr_name', [ATTRS_KEY, METADATA_KEY])
+def test_dataset_item_attr_reserved_name(attr_name):
+    with pytest.raises(ValueError) as excinfo:
+        Dataset.ItemAttr(
+            name=attr_name,
+            shape=(123,),
+            dtype=int)
+    assert ('Item attribute name cannot be %s'
+            % attr_name) in str(excinfo)
+
+
+@pytest.mark.parametrize('attr_name', ['!qwerty', 'asd$gh', 'zxcv&', 'ab.c'])
+def test_dataset_item_attr_name_not_identifier(attr_name):
+    with pytest.raises(ValueError) as excinfo:
+        Dataset.ItemAttr(
+            name=attr_name,
+            shape=None,
+            dtype=int)
+    assert 'name has to be a valid python identifier' in str(excinfo)
+
+
+def test_dataset_init_with_item_attrs(tmp_dataset_path, random_item_attrs):
+    assert not tmp_dataset_path.exists()
+
+    dataset = Dataset(
+        tmp_dataset_path,
+        random_item_attrs)
+
+    assert tmp_dataset_path.exists()
+    assert dataset.path == tmp_dataset_path.resolve()
+
+    item_attrs = dataset.item_attrs
+    assert all(item_attrs[i] == item_attr
+               for i, item_attr in enumerate(
+                   random_item_attrs))
+    assert len(item_attrs) == len(random_item_attrs)
+
+
+def test_dataset_init_with_one_item_attr(tmp_dataset_path, make_random_item_attr):
+
+    item_attr = make_random_item_attr()
+    dataset = Dataset(tmp_dataset_path, item_attr)
+
+    assert len(dataset.item_attrs) == 1
+    assert dataset.item_attrs[0] == item_attr
+
+
+def test_dataset_init_with_metadata(
+        tmp_dataset_path, random_metadata,
+        random_item_attrs):
+
+    dataset = Dataset(
+        tmp_dataset_path,
+        random_item_attrs,
+        metadata=random_metadata)
+
+    for k, v in random_metadata.items():
+        assert getattr(dataset.metadata, k) == v
+
+
+def test_dataset_read_metadata(random_empty_dataset):
+    loaded_metadata = Dataset.read_metadata(random_empty_dataset.path)
+    for k, v in random_empty_dataset.metadata._asdict().items():
+        assert getattr(loaded_metadata, k) == v
+
+
+def test_dataset_read_item_attrs(random_empty_dataset):
+    loaded_item_attrs = Dataset.read_item_attrs(random_empty_dataset.path)
+    assert isinstance(loaded_item_attrs, tuple)
+    assert loaded_item_attrs == random_empty_dataset.item_attrs
+
+
+def test_dataset_add_item(random_empty_dataset, make_random_item_dicts):
+    dataset = random_empty_dataset
+
+    num_items_added = 0
+    assert len(dataset) == num_items_added
+
+    item_dicts = make_random_item_dicts(dataset.item_attrs)
+    for item_dict in item_dicts:
+        dataset.add_item(**item_dict)
+        num_items_added += 1
+        assert len(dataset) == num_items_added
+
+
+def test_dataset_get_item(random_empty_dataset, make_random_item_dicts):
+    dataset = random_empty_dataset
+    item_dicts = make_random_item_dicts(dataset.item_attrs)
+    for item_dict in item_dicts:
+        dataset.add_item(**item_dict)
+
+    for i, item_dict in enumerate(item_dicts):
+        item = dataset[i]
+        for k, v in item_dict.items():
+            assert np.all(getattr(item, k) == v)
+
+
+def test_dataset_iter(random_empty_dataset, make_random_item_dicts):
+    dataset = random_empty_dataset
+    item_dicts = make_random_item_dicts(dataset.item_attrs)
+    for item_dict in item_dicts:
+        dataset.add_item(**item_dict)
+
+    for i, item in enumerate(dataset):
+        item_dict = item_dicts[i]
+        for k, v in item_dict.items():
+            assert np.all(getattr(item, k) == v)
+
+
+def test_dataset_load(random_dataset_with_random_data):
+    base_dataset = random_dataset_with_random_data
+    loaded_dataset = Dataset.load(base_dataset.path)
+
+    assert loaded_dataset.path == base_dataset.path
+    assert loaded_dataset.item_attrs == base_dataset.item_attrs
+    for k, v in base_dataset.metadata._asdict().items():
+        assert getattr(loaded_dataset.metadata, k) == v
+
+    num_items_checked = 0
+    for base_item, loaded_item in zip(base_dataset, loaded_dataset):
+        for item_attr in loaded_dataset.item_attrs:
+            attr_name = item_attr.name
+
+            base_val = getattr(base_item, attr_name)
+            loaded_val = getattr(loaded_item, attr_name)
+            assert np.all(base_val == loaded_val), (np.mean(base_val), np.mean(loaded_val))
+
+        num_items_checked += 1
+
+    assert len(loaded_dataset) == len(base_dataset) == num_items_checked
