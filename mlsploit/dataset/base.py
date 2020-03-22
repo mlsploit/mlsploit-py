0b22da98212f2082dda8d89d8059ef3ec7760f3b
Nilaksh Das <nilakshdas@gatech.edu>
2020-03-22 18:35:47 -0400

    Standardize namedtuple outputs, drop unnecessary property methods

--- mlsploit/dataset/base.py
+++ mlsploit/dataset/base.py
@@ -1,7 +1,8 @@
 from collections import namedtuple
+from functools import partial
 from pathlib import Path
 from threading import Lock
-from typing import Any, Generator, Mapping, \
+from typing import Generator, Mapping, \
     Optional, Sequence, Tuple, Type, Union
 import warnings
 
@@ -21,17 +22,22 @@ _METADATA_KEY = '__METADATA__'
 _RECOMMENDED_FILENAME = 'MLSPLOIT.db'
 
 PrimitiveType = Union[str, int, float, bool]
-MetadataType = Tuple[Any, ...]
-ItemType = Tuple[Any, ...]
+
+# pylint: disable=invalid-name
+ItemViewMeta = partial(namedtuple, 'ItemView')
+ItemSetViewMeta = partial(namedtuple, 'ItemSetView')
+MetadataViewMeta = partial(namedtuple, 'MetadataView')
+ItemAttrSetViewMeta = partial(namedtuple, 'ItemAttrSetView')
+# pylint: enable=invalid-name
 
 
-class _MetaDataset(type):
+class _DatasetMeta(type):
     @property
     def recommended_filename(cls):
         return _RECOMMENDED_FILENAME
 
 
-class Dataset(metaclass=_MetaDataset):
+class Dataset(metaclass=_DatasetMeta):
     class ItemAttr(BaseModel):
         name: str
         shape: Optional[Tuple[int, ...]] = Field(...)
@@ -48,86 +54,75 @@ class Dataset(metaclass=_MetaDataset):
                 raise ValueError('Item attribute name cannot be %s' % v)
             return v
 
-        @validator('name')
-        def _ensure_name_is_identifier(cls, v):
-            # pylint: disable=no-self-argument,no-self-use
-            if not v.isidentifier():
-                raise ValueError('Item attribute name has to be '
-                                 'a valid python identifier (got: %s)' % v)
-            return v
-
     def __init__(self, filepath: FilepathType,
-                 item_attrs: Sequence[ItemAttr],
+                 item_attrs: Sequence['Dataset.ItemAttr'],
                  metadata: Optional[Mapping[str, PrimitiveType]] = None):
 
         if isinstance(item_attrs, self.ItemAttr):
-            item_attrs = [item_attrs]
+            item_attrs = (item_attrs,)
 
         metadata = metadata or dict()
-        self._validate_metadata_dict(metadata)
+        if hasattr(metadata, '_asdict'):
+            metadata = metadata._asdict()
 
-        self._path = Path(filepath).resolve()
-        self._item_attrs = tuple(item_attrs)
-        self._metadata = metadata
+        self.path = Path(filepath).resolve()
+        self.item_attrs = ItemAttrSetViewMeta(
+            map(lambda a: a.name, item_attrs))(*item_attrs)
+        self.metadata = MetadataViewMeta(
+            sorted(metadata.keys()))(**metadata)
+
         self._lock = Lock()
-
-        if not self._path.exists():
+        if not self.path.exists():
             self._setup_dataset()
         self._validate_dataset()
 
     def __repr__(self) -> str:
-        return '%s(\'%s\', item_attrs=%s, metadata=%s, num_items=%d)' % (
+        return '%s(\'%s\', %s, %s, num_items=%d)' % (
             self.__class__.__name__, self.path,
-            self.item_attrs, self.metadata, len(self))
+            self.metadata, self.item_attrs, len(self))
 
     def __len__(self) -> int:
         sizes = set()
-        with self._lock, zarr.ZipStore(self._path, mode='r') as store:
+        with self._lock, zarr.ZipStore(self.path, mode='r') as store:
             root = zarr.group(store=store)
-            for item_attr in self._item_attrs:
+            for item_attr in self.item_attrs:
                 sizes.add(len(root[item_attr.name]))
         if len(sizes) != 1:
             raise RuntimeError('Dataset corrupted!!!')
         return sizes.pop()
 
-    def __getitem__(self, idx) -> ItemType:
-        item_dict = dict()
-        with self._lock, zarr.ZipStore(self._path, mode='r') as store:
+    def __getitem__(self, idx) -> Union['ItemView', 'ItemSetView']:
+        attr_names = list(map(lambda a: a.name, self.item_attrs))
+
+        with self._lock, zarr.ZipStore(self.path, mode='r') as store:
             root = zarr.group(store=store)
-            for item_attr in self._item_attrs:
-                attr_name = item_attr.name
-                item_dict[attr_name] = root[attr_name][idx]
+            data_dict = {attr_name: root[attr_name][idx]
+                         for attr_name in attr_names}
 
-        return namedtuple(
-            'Item', [it.name for it in self._item_attrs])(**item_dict)
+        return ItemViewMeta(attr_names)(**data_dict) \
+            if isinstance(idx, int) \
+            else ItemSetViewMeta(attr_names)(**data_dict)
 
-    def __iter__(self) -> Generator[ItemType, None, None]:
+    def __iter__(self) -> Generator['ItemView', None, None]:
         for i in range(len(self)):
             yield self[i]
 
     def _setup_dataset(self):
-        item_attr_names = set()
-        for item_attr in self._item_attrs:
-            if item_attr.name in item_attr_names:
-                raise RuntimeError(
-                    'Found duplicate item attribute name: %s' % item_attr.name)
-            item_attr_names.add(item_attr.name)
-
-        with self._lock, zarr.ZipStore(self._path, mode='w') as store:
+        with self._lock, zarr.ZipStore(self.path, mode='w') as store:
             root = zarr.group(store=store)
 
             attrs = root.empty(
-                _ATTRS_KEY, shape=len(self._item_attrs),
+                _ATTRS_KEY, shape=len(self.item_attrs),
                 chunks=1, dtype=object, object_codec=Pickle())
-            for i, item_attr in enumerate(self._item_attrs):
+            for i, item_attr in enumerate(self.item_attrs):
                 attrs[i] = item_attr
 
             metadata = root.empty(
                 _METADATA_KEY, shape=1,
                 dtype=object, object_codec=JSON())
-            metadata[0] = self._metadata
+            metadata[0] = self.metadata._asdict()
 
-            for item_attr in self._item_attrs:
+            for item_attr in self.item_attrs:
                 attr_name = item_attr.name
                 attr_shape = item_attr.shape
                 attr_dtype = item_attr.dtype
@@ -142,57 +137,24 @@ class Dataset(metaclass=_MetaDataset):
                     chunks=chunk_shape, dtype=attr_dtype)
 
     def _validate_dataset(self):
-        def dict_equal(dict1, dict2):
-            try:
-                assert set(dict1.keys()) == set(dict2.keys())
-                for key, val in dict1.items():
-                    assert dict2[key] == val
-            except AssertionError:
-                return False
-            else:
-                return True
-
-        with self._lock, zarr.ZipStore(self._path, mode='r') as store:
+        with self._lock, zarr.ZipStore(self.path, mode='r') as store:
             root = zarr.group(store=store)
 
             item_attrs = tuple(root[_ATTRS_KEY])
-            if item_attrs != self._item_attrs:
+            if item_attrs != self.item_attrs:
                 raise RuntimeError('Failed to validate stored dataset, '
                                    'item attributes don\'t match')
 
-            metadata = dict(root[_METADATA_KEY][0])
-            if not dict_equal(metadata, self._metadata):
+            metadata = root[_METADATA_KEY][0]
+            if metadata == self.metadata:
                 raise RuntimeError('Failed to validate stored dataset, '
                                    'metadata doesn\'t match')
-
-    @staticmethod
-    def _validate_metadata_dict(metadata):
-        for k, v in metadata.items():
-            if not k.isidentifier():
-                raise ValueError(f'Metadata key has to be '
-                                 f'a valid python identifier (got: {k})')
-            if not any(isinstance(v, t) for t in PrimitiveType.__args__):
-                raise ValueError(f'Metadata value needs to be '
-                                 f'one of {PrimitiveType.__args__} '
-                                 f'(got: {type(v)})')
-
-    @property
-    def path(self) -> FilepathType:
-        return self._path
-
-    @property
-    def metadata(self) -> MetadataType:
-        return namedtuple('Metadata', self._metadata.keys())(**self._metadata)
-
-    @property
-    def item_attrs(self) -> Tuple['Dataset.ItemAttr', ...]:
-        return tuple(self._item_attrs)
 
     def add_item(self, **attr_kwargs):
         initial_size = len(self)
 
         item_dict = dict()
-        for item_attr in self._item_attrs:
+        for item_attr in self.item_attrs:
             attr_name = item_attr.name
             attr_shape = item_attr.shape
             attr_dtype = item_attr.dtype
@@ -218,7 +180,7 @@ class Dataset(metaclass=_MetaDataset):
         with self._lock, warnings.catch_warnings():
             warnings.simplefilter('ignore', UserWarning)
 
-            store = zarr.ZipStore(self._path, mode='a')
+            store = zarr.ZipStore(self.path, mode='a')
             root = zarr.group(store=store)
             for k, v in item_dict.items():
                 root[k].append(v)
@@ -229,20 +191,21 @@ class Dataset(metaclass=_MetaDataset):
                 'Dataset got corrupted while trying to add %s' % item_dict)
 
     @staticmethod
-    def read_metadata(filepath: FilepathType) -> MetadataType:
+    def read_metadata(filepath: FilepathType) -> 'MetadataView':
         filepath = Path(filepath).resolve()
         with zarr.ZipStore(filepath, mode='r') as store:
             root = zarr.group(store=store)
             metadata = dict(root[_METADATA_KEY][0])
-        return namedtuple('Metadata', metadata.keys())(**metadata)
+        return MetadataViewMeta(metadata.keys())(**metadata)
 
     @staticmethod
-    def read_item_attrs(filepath: FilepathType) -> Tuple['Dataset.ItemAttr', ...]:
+    def read_item_attrs(filepath: FilepathType) -> 'ItemAttrSetView':
         filepath = Path(filepath).resolve()
         with zarr.ZipStore(filepath, mode='r') as store:
             root = zarr.group(store=store)
             item_attrs = tuple(root[_ATTRS_KEY][:])
-        return item_attrs
+        return ItemAttrSetViewMeta(
+            map(lambda a: a.name, item_attrs))(*item_attrs)
 
     @classmethod
     def load(cls, filepath: FilepathType) -> 'Dataset':
